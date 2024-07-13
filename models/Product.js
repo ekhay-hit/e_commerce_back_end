b1613941228df9008e6161e8c41fe85e7fa00081
Houssine <ekhay1@outlook.com>
2024-07-12 22:50:15 -0400

    added foreign key to product model that reference the category table

--- models/Product.js
+++ models/Product.js
@@ -25,7 +25,7 @@ Product.init(
       allowNull: false,
       validate:{
         isDecimal:true,
-      }
+      },
     },
     stock:{
       type: DataTypes.INTEGER,
@@ -33,9 +33,16 @@ Product.init(
       defaultValue:10,
       validate:{
         isNumeric:true,
-      }
+      },
+    },
+    category_id:{
+      type: DataTypes.INTEGER,
+      references:{
+        model: 'category',
+        key: 'id',
+      },
 
-    }
+    },
   },
   {
     sequelize,
