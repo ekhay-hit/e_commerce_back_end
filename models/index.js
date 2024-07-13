17690fc96ef65a5f10309ab43bd1f72bc69d9bec
Houssine <ekhay1@outlook.com>
2024-07-13 15:37:44 -0400

    added relationship between the modles in the index file

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignkey:'category_id',
+  // onDelete: 'CASCADE',
+})
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignkey:'product_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  foreignKey: 'tag_id',
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  foreignKey:'product_id',
+})
 
 module.exports = {
   Product,
